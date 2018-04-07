e0724939e52d92decf1ee3fcd56bd8a9964c7242
NAYHANE <NAYHANENE@HOTMAIL.com>
2018-04-07 11:27:00 +0200

    map container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import MapContainer from './containers/MapContainer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      Map 
+        <MapContainer {...this.props} />
       </div>
     );
   }
