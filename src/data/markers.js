37d56d7b2625fb831b0e9af642b189f790797947
NAYHANE <NAYHANENE@HOTMAIL.com>
2018-04-09 11:55:23 +0200

    rendering markers by redux, with icon and title

---
+++ src/data/markers.js
@@ -0,0 +1,22 @@
+const markers = [
+  {
+    title: "Apple",
+    pointIcon: "https://png.pngtree.com/element_origin_min_pic/00/16/05/08572ef0dc19104.jpg",
+      latLng: {lat: 52.370216,
+      lng:  4.895168}
+  },
+  {
+    title: "Undagrid",
+    pointIcon: "http://www.fundwijzer.nl/wp-content/uploads/2017/08/133858-f854437c-9b34-43b5-b53e-fe8acf7a2b37-logo-medium-1403619985.png",
+      latLng: {lat: 52.293546,
+      lng: 4.739897}
+  },
+  {
+    title: "Codaisseur",
+    pointIcon: "https://i1.wp.com/thenextwomen.com/summit/wp-content/uploads/2017/03/codaisseur-logo-long-2e9b7b7b839a33128016eb77b49ff2bb6397b6b3552523ee09273d6a5b427861.png?fit=1024%2C235",
+      latLng: {lat: 52.339500,
+      lng: 4.856544}
+  }
+]
+
+export default markers
